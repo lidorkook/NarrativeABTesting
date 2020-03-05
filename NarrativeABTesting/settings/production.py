69e6fba62940100dc5a8d7f84e27a7bcacb01b2f
lidorkook <12813806+lidorkook@users.noreply.github.com>
2020-03-05 12:41:35 +0200

    add fabric

--- NarrativeABTesting/settings/production.py
+++ NarrativeABTesting/settings/production.py
@@ -1,1 +1,23 @@
+from .base import *
+
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'narrativeabtesting',
+        'USER': 'narrativeabtesting',
+        'PASSWORD': 'narrativeabtesting',
+        'HOST': '127.0.0.1',
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4',
+        },
+        'CONN_MAX_AGE': 60
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
---
+++ fabfile.py
@@ -0,0 +1,54 @@
+from fabric.api import *
+from contextlib import contextmanager as _contextmanager
+import os, logging, time
+import fabconfig
+
+@_contextmanager
+def virtualenv():
+    with cd(env.venv_path), prefix(env.activate), cd(env.base_directory):
+        yield
+
+def set_debug():
+    logging.basicConfig(level=logging.DEBUG)
+
+def pwd():
+  run("pwd")
+
+def prod():
+    env.env_code = 'prod'
+    env.hosts = ['tools.biggercake.com']    
+    env.base_directory = '/home/ubuntu/narrative/NarrativeABTesting/NarrativeABTesting'
+    env.uwsgi_ini_file = '/etc/uwsgi/apps-enabled/narrative-abtesting.ini'
+    env.venv_path = '/home/ubuntu/narrative/NarrativeABTesting/venv'
+    env.activate = 'source %(venv_path)s/bin/activate' % env
+
+def _restart_uwsgi(uwsgi_ini_file):
+    sudo('touch {}'.format(uwsgi_ini_file))
+
+def _pull_new_version():
+    print('CDing to: {}'.format(env.base_directory))
+    with cd(env.base_directory):
+        run('pwd')
+        run('git pull')
+
+def _restart_uwsgi():
+    sudo('touch {}'.format(env.uwsgi_ini_file))
+
+def _pipfreeze():
+  local("pip freeze > pip.txt")
+
+def _pipinstall():
+    with virtualenv():
+        run("pip install -r pip.txt")
+
+def _makemigrations():
+    with virtualenv():
+        run('python manage.py makemigrations')
+        run('python manage.py migrate')
+
+def deploy():
+    _pipfreeze()
+    _pull_new_version()
+    _pipinstall()
+    _makemigrations()
+    _restart_uwsgi()
